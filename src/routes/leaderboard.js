eca78e824ed41dc7750851d44e5cadf46a07e7b4
santiago <santiagokent@gmail.com>
2019-09-28 17:43:23 -0300

    Added logic to leaderboard route

--- src/routes/leaderboard.js
+++ src/routes/leaderboard.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import { Game } from '../../db';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.json({});
+router.get('/', async (req, res) => {
+  const games = await Game.aggregate([
+    { $group: { _id: '$winner', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+  return res.json(games);
 });
 
 export default router;
